1e8b4ec19baad27952843b68e63d56c2badbb897
ken <ken12034@gmail.com>
2017-02-18 11:24:41 +0800

    brach1

--- footer.php
+++ footer.php
@@ -19,7 +19,7 @@
 		<footer id="colophon" class="site-footer" role="contentinfo">
 			<div class="wrap">
 				<?php
-				get_template_part( 'template-parts/footer/footer', 'widgets' );
+				//get_template_part( 'template-parts/footer/footer', 'widgets' );
 
 				if ( has_nav_menu( 'social' ) ) : ?>
 					<nav class="social-navigation" role="navigation" aria-label="<?php _e( 'Footer Social Links Menu', 'twentyseventeen' ); ?>">
@@ -35,13 +35,13 @@
 					</nav><!-- .social-navigation -->
 				<?php endif;
 
-				get_template_part( 'template-parts/footer/site', 'info' );
+				//get_template_part( 'template-parts/footer/site', 'info' );
 				?>
 			</div><!-- .wrap -->
 		</footer><!-- #colophon -->
 	</div><!-- .site-content-contain -->
 </div><!-- #page -->
-<?php wp_footer(); ?>
+<?php //wp_footer(); ?>
 
 </body>
 </html>
